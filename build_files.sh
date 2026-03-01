f5e031f46827192aec5a7660a96b8ec82345a0e7
Malhar <malharpatil7975@gmail.com>
2026-03-02 00:32:28 +0530

    feat: Update build script to include --break-system-packages for pip installation

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -o errexit
 
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python -m pip install --break-system-packages --upgrade pip
+python -m pip install --break-system-packages -r requirements.txt
 python manage.py collectstatic --noinput
